19dfad59dc5a352da1de8ed70032e8214c1a651c
zhongsheng <2414192895@qq.com>
2017-07-22 15:45:30 +0800

    最小可用

---
+++ manhua/delete.js
@@ -0,0 +1,4 @@
+var bg_child=document.getElementById("layui-layer-shade1"); 
+bg_child.parentNode.removeChild(bg_child); 
+var w_child=document.getElementById('layui-layer1');
+w_child.parentNode.removeChild(w_child);
